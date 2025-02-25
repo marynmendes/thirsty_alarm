bada00ba757d07ce4c4cfdfc58f01a7ff31bcb19
marynmendes <marynicolesm@gmail.com>
2025-02-24 21:53:25 -0300

    Configuração do joystick (simulador de sensor de umidade)

--- thirsty_alarm.c
+++ thirsty_alarm.c
@@ -2,15 +2,22 @@
 #include "pico/stdlib.h"
 #include "hardware/pio.h"
 #include "hardware/clocks.h"
+#include "hardware/adc.h"
 #include "pico/bootrom.h"
 
-
+//arquivo .pio
 #include "thirsty_alarm.pio.h"
 
+#define humidity_sensor 26
 
 int main()
 {
     stdio_init_all();
+    
+    //inicialização do joystick 
+    adc_init();
+    adc_gpio_init(humidity_sensor);  
+    adc_select_input(0);
 
     while (true) {
         printf("Hello, world!\n");
